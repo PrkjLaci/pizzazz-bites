cca8a1f81a2b611b16d7831c28fcfec0c164773c
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-25 14:23:42 +0200

    Add CORS configuration for development environment

--- PizzazzBitesBackend/Program.cs
+++ PizzazzBitesBackend/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using PizzazzBitesBackend.Data;
+using PizzazzBitesBackend.Repository.Pizza;
 using PizzazzBitesBackend.Repository.Pizza.Seeder;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,17 @@ using var scope = app.Services.CreateScope();
 var pizzaSeeder = scope.ServiceProvider.GetRequiredService<IPizzaSeeder>();
 await pizzaSeeder.SeedPizza();
 
+var connection = "http://localhost:5089/";
+app.UseCors(b =>
+{
+    b.WithOrigins(connection!)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithExposedHeaders("content-type")
+        .SetIsOriginAllowed(_ => true);
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -25,6 +38,10 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("Development");
 
 app.UseAuthorization();
 
@@ -37,5 +54,11 @@ void AddServices()
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+     
+    
     builder.Services.AddScoped<IPizzaSeeder, PizzaSeeder>();
+    builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 }
+
+    
+
