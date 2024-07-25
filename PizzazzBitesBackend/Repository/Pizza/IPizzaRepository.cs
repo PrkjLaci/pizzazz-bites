0283470c69a14791760d2a8b4460c383e3746352
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-25 14:24:53 +0200

    Add IPizzaRepository and PizzaRepository for pizza data access

---
+++ PizzazzBitesBackend/Repository/Pizza/IPizzaRepository.cs
@@ -0,0 +1,9 @@
+﻿using PizzazzBitesBackend.Models.Enum;
+
+namespace PizzazzBitesBackend.Repository.Pizza;
+
+public interface IPizzaRepository
+{
+    Task<IEnumerable<Models.Pizza>> GetNewPizzas();
+    Task<IEnumerable<Models.Pizza>> GetPizzasByType(PizzaType pizzaType);
+}
