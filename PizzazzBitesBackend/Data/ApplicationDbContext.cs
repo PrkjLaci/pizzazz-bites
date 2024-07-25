d59a048cd69d7f63e27fd9e578432ad7005bf7f8
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-25 11:14:41 +0200

    Add ApplicationDbContext class for managing database context and entities

---
+++ PizzazzBitesBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PizzazzBitesBackend.Models;
+
+namespace PizzazzBitesBackend.Data;
+
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string>
+{
+    public DbSet<Pizza> Pizzas { get; set; }
+
+public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+{
+}
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<Pizza>()
+        .HasKey(p => p.Id);
+
+    modelBuilder.Entity<Pizza>()
+        .HasIndex(p => p.Name)
+        .IsUnique();
+}
+}
