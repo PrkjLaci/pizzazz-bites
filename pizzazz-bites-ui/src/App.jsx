1a412a34b82dad7cd9a55b86b6ce4dcb945da413
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-30 11:29:50 +0200

    Update App.css and App.jsx

--- pizzazz-bites-ui/src/App.jsx
+++ pizzazz-bites-ui/src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Nav/Navbar";
-import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopNavbar from "./components/Nav/TopNavbar.jsx";
+import SiteHeader from "./components/SiteHeader/SiteHeader.jsx";
 import Home from "./pages/Home/Home.jsx";
 
 function App() {
   return (
     <Router>
       <header>
-        <Navbar />
-        <Header />
+        <TopNavbar />
+        <div className="sticky-top">
+        <SiteHeader/>
+        </div>
       </header>
       <main className="app">
         <Routes>
