c227e005e0d84e623208da9ae5d181a493a1dcbf
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-30 19:45:14 +0200

    Add restaurantImages utility module for managing restaurant carousel images

---
+++ pizzazz-bites-ui/utils/restaurantImages.js
@@ -0,0 +1,28 @@
+const restaurantImages = [
+    {
+        src: "/images/restaurant/restaurant-img-1.jpeg",
+        alt: "First slide",
+        title: "",
+        description: ""
+    },
+    {
+        src: "/images/restaurant/restaurant-img-2.jpeg",
+        alt: "Second slide",
+        title: "",
+        description: ""
+    },
+    {
+        src: "/images/restaurant/restaurant-img-3.jpeg",
+        alt: "Third slide",
+        title: "",
+        description: ""
+    },
+    {
+        src: "/images/restaurant/restaurant-img-4.jpeg",
+        alt: "Fourth slide",
+        title: "",
+        description: ""
+    }
+];
+
+export default restaurantImages;
