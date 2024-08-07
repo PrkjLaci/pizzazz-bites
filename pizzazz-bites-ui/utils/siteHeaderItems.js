160a5b3780e312d676ecb9702688802b9299bd92
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-07 11:23:25 +0200

    Add siteHeaderItems utility module for site header menu items

---
+++ pizzazz-bites-ui/utils/siteHeaderItems.js
@@ -0,0 +1,39 @@
+const siteHeaderItems = [
+  {
+    menuType: "Pizza",
+    itemTypes: [
+      "Italian & International",
+      "Artisan",
+      "Pizza Al Carbone",
+      "Show All",
+    ],
+    route: "/pizzas",
+  },
+  {
+    menuType: "Dessert",
+    itemTypes: ["Italian", "International", "Show All"],
+    route: "/desserts",
+  },
+  {
+    menuType: "Soft Drinks",
+    itemTypes: [],
+    route: "/soft-drinks",
+  },
+  {
+    menuType: "Beer",
+    itemTypes: [],
+    route: "/beer",
+  },
+  {
+    menuType: "Salad / Snacks",
+    itemTypes: [],
+    route: "/salad-snacks",
+  },
+  {
+    menuType: "Dressing / Cheese",
+    itemTypes: [],
+    route: "/dressing-cheese",
+  },
+];
+
+export default siteHeaderItems;
