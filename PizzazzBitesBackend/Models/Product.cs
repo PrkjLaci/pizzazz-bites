bb4df098173adafacc64363f657bee88071cad99
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-20 10:30:12 +0200

    Add Product model

---
+++ PizzazzBitesBackend/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzazzBitesBackend.Models;
+
+public abstract class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Ingredients { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public bool IsNew { get; set; }
+    public decimal Rating { get; set; }
+    public int RatingCount { get; set; }
+}
