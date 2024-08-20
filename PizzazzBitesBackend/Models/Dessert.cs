6ad6ed18224d31bbe67a9ce831aec33a20d7374c
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-20 10:30:34 +0200

    Refactor Dessert and Pizza models to inherit from Product

--- PizzazzBitesBackend/Models/Dessert.cs
+++ PizzazzBitesBackend/Models/Dessert.cs
@@ -2,18 +2,9 @@
 
 namespace PizzazzBitesBackend.Models;
 
-public class Dessert
+public class Dessert : Product
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
-    public string? Ingredients { get; set; }
-    public string ImageUrl { get; set; }
-    public decimal Price { get; set; }
     public bool IsLactoseFree { get; set; }
     public bool IsGlutenFree { get; set; }
-    public bool IsNew { get; set; }
     public DessertType DessertType { get; set; }
-    public decimal Rating { get; set; }
-    public int RatingCount { get; set; }
 }
