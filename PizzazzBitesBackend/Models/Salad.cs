11f342220dc3c31490767a055291f7f9d9fb964b
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 13:26:10 +0200

    Add Salad model and SaladType enum

---
+++ PizzazzBitesBackend/Models/Salad.cs
@@ -0,0 +1,13 @@
+﻿using PizzazzBitesBackend.Models.Enum;
+
+namespace PizzazzBitesBackend.Models;
+
+public class Salad : Product
+{
+    public int ProductId { get; set; }
+    public bool IsVegan { get; set; }
+    public bool IsVegetarian { get; set; }
+    public bool IsGlutenFree { get; set; }
+    public bool IsContainsNuts { get; set; }
+    public SaladType SaladType { get; set; }
+}
