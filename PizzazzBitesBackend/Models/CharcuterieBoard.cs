ae7bef1b8679a350198635b8a536193c60c22ab9
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 14:43:34 +0200

    Add CharcuterieBoard model

---
+++ PizzazzBitesBackend/Models/CharcuterieBoard.cs
@@ -0,0 +1,10 @@
+﻿using PizzazzBitesBackend.Models.Enum;
+
+namespace PizzazzBitesBackend.Models;
+
+public class CharcuterieBoard : Product
+{
+    public int ProductId { get; set; }
+    public string ServingSize { get; set; }
+    public CharcuterieBoardType CharcuterieBoardType { get; set; }
+}
