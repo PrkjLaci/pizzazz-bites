0725e7a03a7e2070f5c729e79cca7cba11a8efd5
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 12:39:09 +0200

    Update fetchMenuItems.js to handle additional drink subtypes

--- pizzazz-bites-ui/utils/fetchMenuItems.js
+++ pizzazz-bites-ui/utils/fetchMenuItems.js
@@ -9,6 +9,9 @@ const fetchMenuItems = async (
   if (clickedSubType === "Show All") clickedSubType = "";
   if (clickedSubType === "Italian & International") clickedSubType = "ItalianAndInternational";
   if (clickedSubType === "Pizza Al Carbone") clickedSubType = "PizzaAlCarbone";
+  if (clickedSubType === "Soft Drinks") clickedSubType = "SoftDrink";
+  if (clickedSubType === "Short Drinks") clickedSubType = "ShortDrink";
+
 
   const pageSize = 10;
   console.log("fetchMenuItems", clickedSubType, page, productType);
