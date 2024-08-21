af00147929e9c1c047ef092c142dab3b932ee04f
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 12:38:17 +0200

    Add properties to Drink model for alcohol percentage, sugar-free status, and volume

--- PizzazzBitesBackend/Models/Drink.cs
+++ PizzazzBitesBackend/Models/Drink.cs
@@ -7,5 +7,8 @@ public class Drink : Product
     public int ProductId { get; set; }
     public bool IsCarbonated { get; set; }
     public bool IsAlcoholic { get; set; }
+    public decimal AlcoholPercentage { get; set; }
+    public bool IsSugarFree { get; set; }
+    public string Volume { get; set; }
     public DrinkType DrinkType { get; set; }
 }
