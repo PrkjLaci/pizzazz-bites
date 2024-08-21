e1b3e53f6c7fcad606a6ed7c201c3c55b1c0fd7a
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 07:04:04 +0200

    Refactor Pizza model to include ProductId property

--- PizzazzBitesBackend/Models/Pizza.cs
+++ PizzazzBitesBackend/Models/Pizza.cs
@@ -3,6 +3,7 @@
 namespace PizzazzBitesBackend.Models;
 public class Pizza : Product
 {
+        public int ProductId { get; set; }
         public bool IsSpicy { get; set; }
         public bool IsVegetarian { get; set; }
         public PizzaType PizzaType { get; set; }
