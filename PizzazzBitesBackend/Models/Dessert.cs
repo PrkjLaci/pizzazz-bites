7f489ad47ce8e87fff053a047d1846193f4005ff
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 07:03:52 +0200

    Refactor Dessert model to include ProductId property

--- PizzazzBitesBackend/Models/Dessert.cs
+++ PizzazzBitesBackend/Models/Dessert.cs
@@ -4,6 +4,7 @@ namespace PizzazzBitesBackend.Models;
 
 public class Dessert : Product
 {
+    public int ProductId { get; set; }
     public bool IsLactoseFree { get; set; }
     public bool IsGlutenFree { get; set; }
     public DessertType DessertType { get; set; }
