cc896935826c768d0de058d95ff2df4a70df677d
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-21 08:02:00 +0200

    Add Drink model

---
+++ PizzazzBitesBackend/Models/Drink.cs
@@ -0,0 +1,11 @@
+﻿using PizzazzBitesBackend.Models.Enum;
+
+namespace PizzazzBitesBackend.Models;
+
+public class Drink : Product
+{
+    public int ProductId { get; set; }
+    public bool IsCarbonated { get; set; }
+    public bool IsAlcoholic { get; set; }
+    public DrinkType DrinkType { get; set; }
+}
