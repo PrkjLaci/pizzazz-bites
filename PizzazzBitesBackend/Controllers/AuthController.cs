9933e4deb0051092f9267b580856622e79805741
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-22 18:15:49 +0200

    Add AuthController for user authentication and registration

---
+++ PizzazzBitesBackend/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PizzazzBitesBackend.Contracts;
+using PizzazzBitesBackend.Services.Authentication;
+
+namespace PizzazzBitesBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authService.RegisterAsync(request);
+        
+        if (!result.Success)
+        {
+            GetAddErrors(result);
+            return BadRequest(result);
+        }
+
+        return CreatedAtAction(nameof(Register),
+            new RegistrationResponse(result.FirstName, result.LastName, result.Email);
+    }
+    
+    private void GetAddErrors(AuthResult result)
+    {
+        foreach (var error in result.ErrorMessages)
+        {
+            ModelState.AddModelError(error.Key, error.Value);
+        }
+    }
+}
