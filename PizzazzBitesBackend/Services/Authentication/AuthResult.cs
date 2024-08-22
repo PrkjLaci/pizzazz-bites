4612c4404361e038047fa709dfaa5d761ce9d8fa
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-22 18:14:53 +0200

    Add AuthResult class for authentication

---
+++ PizzazzBitesBackend/Services/Authentication/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzazzBitesBackend.Services.Authentication;
+
+public record AuthResult(
+    bool Success,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Token)
+{
+    //Error code - error message
+    public readonly Dictionary<string, string> ErrorMessages = new();
+}
