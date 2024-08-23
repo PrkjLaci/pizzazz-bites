093b11ecea734be3f0dbbffe30e85f7045810f71
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-23 09:08:47 +0200

    Refactor AuthController and add Login endpoint

--- PizzazzBitesBackend/Controllers/AuthController.cs
+++ PizzazzBitesBackend/Controllers/AuthController.cs
@@ -16,7 +16,7 @@ public class AuthController : ControllerBase
         _authService = authService;
     }
 
-    [HttpPost("register")]
+    [HttpPost("Register")]
     public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
     {
             if(!ModelState.IsValid)
@@ -28,15 +28,33 @@ public class AuthController : ControllerBase
         
             if (!result.Success)
             {
-                GetAddErrors(result);
+                AddErrors(result);
                 return BadRequest(result);
             }
 
             return CreatedAtAction(nameof(Register),
                 new RegistrationResponse(result.FirstName, result.LastName, result.Email));
     }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var result = await _authService.LoginAsync(request.Email, request.Password);
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+        
+        return Ok(new AuthResponse(result.Email, result.FirstName, result.LastName, result.Token));
+    }
     
-    private void GetAddErrors(AuthResult result)
+    private void AddErrors(AuthResult result)
     {
         foreach (var error in result.ErrorMessages)
         {
