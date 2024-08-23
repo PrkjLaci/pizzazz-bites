f48c2bd2dc36c35762107dfb231c8641d1f973df
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-23 10:25:52 +0200

    refactor AuthenticationSeeder to include admin creation if not exists

--- PizzazzBitesBackend/Services/Authentication/AuthenticationSeeder.cs
+++ PizzazzBitesBackend/Services/Authentication/AuthenticationSeeder.cs
@@ -1,21 +1,22 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using PizzazzBitesBackend.Models;
 
 namespace PizzazzBitesBackend.Services.Authentication;
 
 public class AuthenticationSeeder
 {
     private readonly RoleManager<IdentityRole> _roleManager;
-    
-    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager)
+    private readonly UserManager<User> _userManager;
+    public AuthenticationSeeder(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
     {
         _roleManager = roleManager;
+        _userManager = userManager;
     }
 
     public void AddRoles()
     {
         var tAdmin = CreateAdminRole(_roleManager);
         tAdmin.Wait();
-
         var tUser = CreateUserRole(_roleManager);
         tUser.Wait();
     }
@@ -28,5 +29,26 @@ public class AuthenticationSeeder
     async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
     {
         await roleManager.CreateAsync(new IdentityRole("User"));
+    }
+
+    public void AddAdmin()
+    {
+        var tAdmin = CreateAdminIfNotExists();
+        tAdmin.Wait();
+    }
+    
+    private async Task CreateAdminIfNotExists()
+    {
+        var adminInDb = await _userManager.FindByEmailAsync("admin@admin.com");
+        if (adminInDb == null)
+        {
+            var admin = new User { UserName = "admin", Email = "admin@admin.com", FirstName = "Admin", LastName = "Admin"};
+            var adminCreated = await _userManager.CreateAsync(admin, "admin123");
+
+            if (adminCreated.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
     }
 }
