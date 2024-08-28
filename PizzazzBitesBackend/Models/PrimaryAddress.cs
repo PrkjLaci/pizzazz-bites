6374797a2114622ed0aa2f079b61deb5fee3f120
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-28 13:50:11 +0200

    Add PrimaryAddress model for managing user addresses

---
+++ PizzazzBitesBackend/Models/PrimaryAddress.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PizzazzBitesBackend.Models;
+
+public class PrimaryAddress
+{
+    public int? PrimaryAddressId { get; set; }
+    public string HouseNumber { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+    
+    public string UserId { get; set; }
+    [JsonIgnore]
+    public User User { get; set; }
+}
