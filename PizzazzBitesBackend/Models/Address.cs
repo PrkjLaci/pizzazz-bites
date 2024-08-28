c4dd98a13ffbcf9ccf66a4c113213ee745463920
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-28 13:34:21 +0200

    Update Address model with key attribute and add JSON ignore for User property

--- PizzazzBitesBackend/Models/Address.cs
+++ PizzazzBitesBackend/Models/Address.cs
@@ -1,7 +1,11 @@
-﻿namespace PizzazzBitesBackend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PizzazzBitesBackend.Models;
 
 public class Address
 {
+    [Key]
     public int Id { get; set; }
     public string HouseNumber { get; set; }
     public string Street { get; set; }
@@ -9,7 +13,7 @@ public class Address
     public string State { get; set; }
     public string ZipCode { get; set; }
     public string Country { get; set; }
-    
     public string UserId { get; set; }
-    public User User { get; set; }
+    [JsonIgnore]
+    public User? User { get; set; }
 }
