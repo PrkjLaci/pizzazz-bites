63dc1f878f5a94e135c050c5e80018d6bc8eaf3c
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-28 13:33:16 +0200

    Add AddressRepository and IAddressRepository for managing user addresses

---
+++ PizzazzBitesBackend/Repository/Address/IAddressRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzazzBitesBackend.Repository.Address;
+
+public interface IAddressRepository
+{
+    public Task AddAddressToUser(string email, Models.Address address);
+    public Task<ICollection<Models.Address>?> GetAllAddresses(string email);
+    public Task UpdateAddressOrder
+    public Task SelectPrimaryAddress(string email, int addressId);
+}
