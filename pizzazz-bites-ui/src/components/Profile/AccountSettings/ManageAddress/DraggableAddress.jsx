c6a20afb0e4fff63b81ab38283e518e8b9c5df01
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-08-30 07:47:06 +0200

    Update DraggableAddress component to display full address details and add AddressActionsDropDown

--- pizzazz-bites-ui/src/components/Profile/AccountSettings/ManageAddress/DraggableAddress.jsx
+++ pizzazz-bites-ui/src/components/Profile/AccountSettings/ManageAddress/DraggableAddress.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBTypography,
-} from "mdb-react-ui-kit";
+import React, { useState } from "react";
+import { MDBCard, MDBCardBody, MDBTypography, MDBBtn } from "mdb-react-ui-kit";
 import { CSS } from "@dnd-kit/utilities";
 import { useSortable } from "@dnd-kit/sortable";
 import "./DraggableAddress.css";
+import AddressActionsDropDown from "./AddressActionsDropDown";
 
 const DraggableAddress = ({ id, address, addresses }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -20,21 +17,28 @@ const DraggableAddress = ({ id, address, addresses }) => {
   };
 
   return (
-    <MDBCard ref={setNodeRef} {...attributes} {...listeners} style={style}>
-      <MDBCardBody
-        style={
-          addresses[0].id === address.id
-            ? { backgroundColor: "rgb(80, 200, 120)" }
-            : { backgroundColor: "rgb(211, 211, 211)" }
-        }
-        className="address-info"
-      >
-        <MDBTypography blockquote className="mb-0">
-          <h6>{addresses[0].id === address.id && "Shipping address:"}</h6>
-          <p>{`${address.houseNumber} ${address.street}`}</p>
-        </MDBTypography>
-      </MDBCardBody>
-    </MDBCard>
+    <>
+      <MDBCard ref={setNodeRef} {...attributes} {...listeners} style={style}>
+        <MDBCardBody
+          style={
+            addresses[0].id === address.id
+              ? { backgroundColor: "rgb(80, 200, 120)" }
+              : { backgroundColor: "rgb(211, 211, 211)" }
+          }
+          className="address-info"
+        >
+          <MDBTypography blockquote className="mb-0">
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div>
+                <h6>{addresses[0].id === address.id && "Shipping address:"}</h6>
+                <p>{`${address.street} ${address.houseNumber}, ${address.city}, ${address.state}, ${address.zipCode}, ${address.country}`}</p>
+              </div>
+              <AddressActionsDropDown />
+            </div>
+          </MDBTypography>
+        </MDBCardBody>
+      </MDBCard>
+    </>
   );
 };
 
