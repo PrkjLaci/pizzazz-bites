8a4c8a85403921b0e31907ef07f98dc632c4633d
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-12 09:15:13 +0200

    Add CartProduct model for managing products in the user's shopping cart

---
+++ PizzazzBitesBackend/Models/Cart/CartProduct.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PizzazzBitesBackend.Models.Cart;
+
+public class CartProduct
+{
+    [JsonIgnore]
+    public int CartId { get; set; }
+    [JsonIgnore]
+    public Cart Cart { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+}
