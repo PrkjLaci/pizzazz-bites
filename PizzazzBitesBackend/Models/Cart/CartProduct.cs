9a0418b89ad80d34cc31654af43c8194848065db
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-13 07:40:29 +0200

    Refactor CartProduct model to allow for nullable Product property

--- PizzazzBitesBackend/Models/Cart/CartProduct.cs
+++ PizzazzBitesBackend/Models/Cart/CartProduct.cs
@@ -9,6 +9,6 @@ public class CartProduct
     [JsonIgnore]
     public Cart Cart { get; set; }
     public int ProductId { get; set; }
-    public Product Product { get; set; }
+    public Product? Product { get; set; }
     public int Quantity { get; set; }
 }
