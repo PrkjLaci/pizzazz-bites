205e7b2298e44b52d1933152b278df2a0a651826
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-13 07:42:52 +0200

    Refactor ItemTable component to add AddItemToCart functionality

--- pizzazz-bites-ui/src/components/ItemTable/ItemTable.jsx
+++ pizzazz-bites-ui/src/components/ItemTable/ItemTable.jsx
@@ -7,59 +7,92 @@ import {
   MDBTableBody,
 } from "mdb-react-ui-kit";
 import "./ItemTable.css";
-
+import React, { useState } from "react";
+import AddItemToCart from "../Cart/AddItemToCart";
 
 const ItemTable = ({ itemName, items, clickedItemType, page }) => {
+  const [showAddItemToCart, setShowAddItemToCart] = useState(false);
+  const [clickedItem, setClickedItem] = useState({});
+
+  const toggleAddItemToCart = () => setShowAddItemToCart(!showAddItemToCart);
+
+  console.log("clickedItem", clickedItem);
+
   return (
-    <MDBTable align="middle">
-      <MDBTableHead>
-        <tr>
-          <th scope="col" className="item-table-nav">
-            {itemName} » {clickedItemType === "" ? "Show All" : clickedItemType}{" "}
-            » {page} page
-          </th>
-          <th scope="col"></th>
-          <th scope="col"></th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        {items.map((item) => (
-          <tr key={item.id} className="m-5 table-img">
-            <td className="talbe-image">
-              <img
-                src={item.imageUrl}
-                alt=""
-                className="item-image"
-              />
-            </td>
-            <td className="table-description">
-              <h4 className="fw-bold mb-0">
-                {item.productId <= 9 ? `0${item.productId}. ` : `${item.productId}. `}
-                {item.name}
-              </h4>
-              <p className="mb-1">
-                <i>{item.ingredients ? `Ingredients: ${item.ingredients}` : ""}</i>
-              </p>
-              <p className="mb-1">
-                <i>{item.description}</i>
-              </p>
-              <p className="mb-1">
-                <i>{item.isAlcoholic ? `Alcohol percentage: ${item.alcoholPercentage}%` : ""}</i>
-              </p>
-              <p className="mb-1">
-                <i>{item.volume}</i>
-              </p>
-            </td>
-            <td className="table-pricing">
-              {item.price}.-
-              <MDBBtn color="" rounded size="sm" className="cart-button">
-                <RiShoppingCartLine />
-              </MDBBtn>
-            </td>
+    <>
+      <MDBTable align="middle">
+        <MDBTableHead>
+          <tr>
+            <th scope="col" className="item-table-nav">
+              {itemName} »{" "}
+              {clickedItemType === "" ? "Show All" : clickedItemType} » {page}{" "}
+              page
+            </th>
+            <th scope="col"></th>
+            <th scope="col"></th>
           </tr>
-        ))}
-      </MDBTableBody>
-    </MDBTable>
+        </MDBTableHead>
+        <MDBTableBody>
+          {items.map((item) => (
+            <tr key={item.id} className="m-5 table-img">
+              <td className="talbe-image">
+                <img src={item.imageUrl} alt="" className="item-image" />
+              </td>
+              <td className="table-description">
+                <h4 className="fw-bold mb-0">
+                  {item.productId <= 9
+                    ? `0${item.productId}. `
+                    : `${item.productId}. `}
+                  {item.name}
+                </h4>
+                <p className="mb-1">
+                  <i>
+                    {item.ingredients ? `Ingredients: ${item.ingredients}` : ""}
+                  </i>
+                </p>
+                <p className="mb-1">
+                  <i>{item.description}</i>
+                </p>
+                <p className="mb-1">
+                  <i>
+                    {item.isAlcoholic
+                      ? `Alcohol percentage: ${item.alcoholPercentage}%`
+                      : ""}
+                  </i>
+                </p>
+                <p className="mb-1">
+                  <i>{item.volume}</i>
+                </p>
+              </td>
+              <td className="table-pricing">
+                {item.price}.-
+                <MDBBtn
+                  color=""
+                  rounded
+                  size="sm"
+                  className="cart-button"
+                  onClick={() => {
+                    setClickedItem(item);
+                    toggleAddItemToCart();
+                  }}
+                >
+                  <RiShoppingCartLine />
+                </MDBBtn>
+              </td>
+            </tr>
+          ))}
+        </MDBTableBody>
+      </MDBTable>
+      {showAddItemToCart && (
+        <AddItemToCart
+          showAddItemToCart={showAddItemToCart}
+          setShowAddItemToCart={setShowAddItemToCart}
+          toggleAddItemToCart={toggleAddItemToCart}
+          setClickedItem={setClickedItem}
+          clickedItem={clickedItem}
+        />
+      )}
+    </>
   );
 };
 
