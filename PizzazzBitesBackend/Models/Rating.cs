5e0c4cb2cdc890a905d6a5354e0154552ff31143
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-19 11:15:38 +0200

    Add Rating model class

---
+++ PizzazzBitesBackend/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzazzBitesBackend.Models;
+
+public class Rating
+{
+    public string UserId { get; set; }
+    public User User { get; set; }
+    
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    
+    public int Value { get; set; }
+}
