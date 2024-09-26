496d3ad02620c9ddd334ce5be4e00d176fae17de
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-26 09:30:36 +0200

    Refactor AddressRepository

--- PizzazzBitesBackend/Repository/Address/AddressRepository.cs
+++ PizzazzBitesBackend/Repository/Address/AddressRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 using PizzazzBitesBackend.Data;
 using PizzazzBitesBackend.Models;
 
@@ -8,6 +9,8 @@ public class AddressRepository : IAddressRepository
 {
     private readonly ApplicationDbContext _context;
     private readonly ILogger<AddressRepository> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private string UserId => _httpContextAccessor.HttpContext.User.FindAll(ClaimTypes.NameIdentifier).Last().Value;
     
     public AddressRepository(ApplicationDbContext context, ILogger<AddressRepository> logger)
     {
@@ -38,9 +41,23 @@ public class AddressRepository : IAddressRepository
         return null;
     }
 
+    public async Task<PrimaryAddress?> GetPrimaryAddress()
+    {
+        try
+        {
+            var primaryAddress = await _context.PrimaryAddresses.FirstOrDefaultAsync(pa => pa.UserId == UserId);
+            return primaryAddress;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            throw new Exception("An error occurred while getting the primary address. Please try again.");
+        }
+    }
+
     public async Task RefreshAddressOrder(string email, List<Models.Address> addresses)
     {
-        var user = await _context.Users.Include(u => u.Addresses).FirstOrDefaultAsync(u => u.Email == email);
+        var user = await _context.Users.Include(u => u.Addresses).Include(user => user.PrimaryAddress).FirstOrDefaultAsync(u => u.Email == email);
         if (user != null)
         {
             foreach (var address in addresses)
@@ -51,29 +68,32 @@ public class AddressRepository : IAddressRepository
                     existingAddress.Order = address.Order;
                 }
             }
-            await _context.SaveChangesAsync();
-        }
-    }
 
-    public async Task SelectPrimaryAddress(string email, int addressId)
-    {
-        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
-        if (user != null)
-        {
-            var address = await _context.Addresses.FirstOrDefaultAsync(a => a.Id == addressId);
-            if (address != null)
+            if (user.Addresses != null)
             {
+                var firstAddress = user.Addresses.FirstOrDefault(a => a.Order == 0);
+                if(user.PrimaryAddress != null)
+                {
+                    user.PrimaryAddress.HouseNumber = firstAddress.HouseNumber;
+                    user.PrimaryAddress.Street = firstAddress.Street;
+                    user.PrimaryAddress.City = firstAddress.City;
+                    user.PrimaryAddress.State = firstAddress.State;
+                    user.PrimaryAddress.ZipCode = firstAddress.ZipCode;
+                    user.PrimaryAddress.Country = firstAddress.Country;
+                }
+                
                 user.PrimaryAddress = new PrimaryAddress
                 {
-                    HouseNumber = address.HouseNumber,
-                    Street = address.Street,
-                    City = address.City,
-                    State = address.State,
-                    ZipCode = address.ZipCode,
-                    Country = address.Country
+                    HouseNumber = firstAddress.HouseNumber,
+                    Street = firstAddress.Street,
+                    City = firstAddress.City,
+                    State = firstAddress.State,
+                    ZipCode = firstAddress.ZipCode,
+                    Country = firstAddress.Country
                 };
-                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
             }
+
             await _context.SaveChangesAsync();
         }
     }
