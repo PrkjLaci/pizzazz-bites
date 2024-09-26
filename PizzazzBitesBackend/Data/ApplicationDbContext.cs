a952bd59998608c9c2a55407271e5e67b450f71d
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-26 09:29:05 +0200

    Add Order and OrderProduct models and configure relationships

--- PizzazzBitesBackend/Data/ApplicationDbContext.cs
+++ PizzazzBitesBackend/Data/ApplicationDbContext.cs
@@ -21,6 +21,8 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string
     public DbSet<Cart> Carts { get; set; }
     public DbSet<CartProduct> CartProducts { get; set; }
     public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
@@ -76,7 +78,7 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string
         
         modelBuilder.Entity<CartProduct>()
             .HasKey(cp => new { cp.CartId, cp.ProductId });
-        
+
         modelBuilder.Entity<CartProduct>()
             .HasOne(cp => cp.Cart)
             .WithMany(c => c.CartProducts)
@@ -86,6 +88,19 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string
             .HasOne(cp => cp.Product)
             .WithMany(p => p.CartProducts)
             .HasForeignKey(cp => cp.ProductId);
+
+        modelBuilder.Entity<OrderProduct>()
+            .HasKey(op => new { op.OrderId, op.ProductId});
+
+        modelBuilder.Entity<OrderProduct>()
+            .HasOne(op => op.Order)
+            .WithMany(o => o.OrderProducts)
+            .HasForeignKey(op => op.OrderId);
+        
+        modelBuilder.Entity<OrderProduct>()
+            .HasOne(op => op.Product)
+            .WithMany(p => p.OrderProducts)
+            .HasForeignKey(op => op.ProductId);
         
         modelBuilder.Entity<Rating>()
             .HasKey(r => new { r.UserId, r.ProductId });
