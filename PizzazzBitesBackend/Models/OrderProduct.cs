bc54bc9f3476c20282162ad9cdb765cd9b40f5fc
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-26 09:25:24 +0200

    Add OrderProduct model for managing order items

---
+++ PizzazzBitesBackend/Models/OrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using PizzazzBitesBackend.Models.Cart;
+
+namespace PizzazzBitesBackend.Models;
+
+public class OrderProduct
+{ 
+    public int? OrderId { get; set; }
+    [JsonIgnore]
+    public Order? Order { get; set; }
+   
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+    public int Quantity { get; set; }
+}
