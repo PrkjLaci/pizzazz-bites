5e17a1c32d3ff939203353413d52129cb3eb7c6f
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-26 09:28:58 +0200

    Add OrderRepository interface and implementation

---
+++ PizzazzBitesBackend/Repository/Order/IOrderRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace PizzazzBitesBackend.Repository.Order;
+
+public interface IOrderRepository
+{
+    public Task<Models.Order> AddOrder(Models.Order order);
+    public Task<ICollection<Models.Order>> GetOrdersByUser();
+}
