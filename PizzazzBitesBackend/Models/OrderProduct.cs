d555cd9b8f491d8ca6410f0b7beb4a2164512f36
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-09-26 09:28:22 +0200

    Refactor OrderProduct model imports

--- PizzazzBitesBackend/Models/OrderProduct.cs
+++ PizzazzBitesBackend/Models/OrderProduct.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
-using System.Text.Json.Serialization;
-using PizzazzBitesBackend.Models.Cart;
+﻿using System.Text.Json.Serialization;
 
 namespace PizzazzBitesBackend.Models;
 
