8e2efcc0b204bc654b9e0dc79327500f88f5a676
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-10-04 12:10:37 +0200

    Add LoyaltyPointController with endpoints to get and use loyalty points

---
+++ PizzazzBitesBackend/Controllers/LoyaltyPointController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PizzazzBitesBackend.Repository.LoyaltyPoint;
+
+namespace PizzazzBitesBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoyaltyPointController : ControllerBase
+{
+    private readonly ILoyaltyPointRepository _loyaltyPointRepository;
+    private readonly ILogger<LoyaltyPointController> _logger;
+
+    public LoyaltyPointController(ILoyaltyPointRepository loyaltyPointRepository, ILogger<LoyaltyPointController> logger)
+    {
+        _loyaltyPointRepository = loyaltyPointRepository;
+        _logger = logger;
+    }
+    
+    [Authorize (Roles = "User")]
+    [HttpGet("get-loyalty-points")]
+    public async Task<ActionResult> GetLoyaltyPoints()
+    {
+        try
+        {
+            var loyaltyPoints = await _loyaltyPointRepository.GetLoyaltyPoints();
+            return Ok(new { message = "Loyalty points found successfully.", data = loyaltyPoints });
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new { message = e.Message });
+        }
+    }
+    
+    [Authorize (Roles = "User")]
+    [HttpPost("use-loyalty-points")]
+    public async Task<ActionResult>  UseLoyaltyPoints([FromBody] decimal pointsToUse)
+    {
+        try
+        {
+            await _loyaltyPointRepository.UseLoyaltyPoint(pointsToUse);
+            return Ok(new { message = "Loyalty points used successfully." });
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new { message = e.Message });
+        }
+    }
+}
