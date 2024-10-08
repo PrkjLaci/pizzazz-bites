3fc799ab2a07a02ed54f92b8cc3def9fb1c5ce75
PrkjLaci <prokaj.laszlo96@gmail.com>
2024-10-08 14:21:30 +0200

    Add ContactUsController for sending contact us emails

---
+++ PizzazzBitesBackend/Controllers/ContactUsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PizzazzBitesBackend.Models.SMTP;
+using PizzazzBitesBackend.Services.SMTP;
+
+namespace PizzazzBitesBackend.Controllers;
+
+[ApiController]
+[Route ("api/[controller]")]
+public class ContactUsController : ControllerBase
+{
+    private readonly ILogger<ContactUsController> _logger;
+    private readonly IContactUs _contactUs;
+
+    public ContactUsController(ILogger<ContactUsController> logger, IContactUs contactUs)
+    {
+        _logger = logger;
+        _contactUs = contactUs;
+    }
+
+    [HttpPost ("contact-us")]
+    public async Task<IActionResult> SendEmail([FromBody] ContactUsEmailModel contactUsEmailModel)
+    {
+        try
+        {
+            var emailSent = await _contactUs.SendEmail(contactUsEmailModel);
+            if (!emailSent)
+            {
+                return BadRequest(new { message = "Error sending email" });
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(new { message = e.Message });
+        }
+
+        return Ok(new { message = "Email sent successfully" });
+    }
+}
